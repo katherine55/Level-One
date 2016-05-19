7f69b4fd82adb17d24f2db27580b1b3893fb9600
Love4653 <gabrieltcampbell@icloud.com>
2016-05-18 20:02:13 -0700

    new
    new

---
+++ src/GetFriends.java
@@ -0,0 +1,20 @@
+public class GetFriends {
+
+
+	public static void main(String[] args){
+    	Classmate ella = new Classmate();
+    	Classmate kayia = new Classmate();//4. set descriptions for each Classmate
+
+   	 //5. Use your getter to print out each description
+	}
+
+}
+
+class Classmate{
+    String description;
+    
+    
+    //1. create a setter that sets a description this person
+    //2. create a getter that gets a description of this person
+
+}
---
+++ src/TurtlePond.java
@@ -0,0 +1,165 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import org.teachingextensions.logo.Paintable;
+import org.teachingextensions.logo.PenColors;
+import org.teachingextensions.logo.Tortoise;
+
+public class TurtlePond implements KeyEventDispatcher {
+
+	int cookieX = 345;
+	int cookieY = 92;
+
+	int speed = 5;
+
+	void setup() {
+		JOptionPane.showMessageDialog(null,
+				"Move the turtle with the arrow keys to find the hidden cookie.\nYou'll get warmer the closer you get.\n\nHurry or she'll die of hunger!");
+
+		// 4. For debugging purposes, show the cookie. The user won’t see this.
+	}
+
+	private void goUp() {
+		Tortoise.move(speed);
+	}
+
+	private void goDown() {
+		Tortoise.move(-speed);
+	}
+
+	private void goLeft() {
+		Tortoise.turn(-90);
+		Tortoise.move(speed);
+		Tortoise.turn(90);
+
+	}
+
+	private void goRight() {
+		Tortoise.turn(90);
+		Tortoise.move(speed);
+		Tortoise.turn(-90);
+	}
+
+	private void checkForFood() throws Exception {
+		int tortoiseLocationX = Tortoise.getX();
+		int tortoiseLocationY = Tortoise.getY();
+
+		if(Math.abs(tortoiseLocationX - cookieX) <= 100  && Math.abs(tortoiseLocationY - cookieY) <= 100){
+			setBackgroundColor(Color.yellow);
+		}
+		// 7. If the Tortoise is within 100 pixels of the food, set the
+		// background color to yellow
+		if(Math.abs(tortoiseLocationX - cookieX) <= 50 && Math.abs(tortoiseLocationY - cookieY) <= 50 ){
+			setBackgroundColor(Color.orange);
+		}
+		// 8. If the Tortoise is within 50 pixels of the food, set the
+		// background color to orange
+		if(Math.abs(tortoiseLocationX - cookieX) <= 20 && Math.abs(tortoiseLocationY - cookieY) <= 20){
+			setBackgroundColor(Color.red);
+		}
+		// 9. If the Tortoise is within 20 pixels of the food, set the
+		// background color to red
+		if(Math.abs(tortoiseLocationX - cookieX) <= 5 && Math.abs(tortoiseLocationY - cookieY) <= 5){
+			JOptionPane.showMessageDialog(null, "You found the cookie! YUM YUM YUM");
+		}
+		// 10. If the Tortoise is within 5 pixels of the cookie, make a pop-up
+		// to tell them they found it
+if(getTimeElapsed() >= 50){
+	JOptionPane.showMessageDialog(null, "The turtle died because it was too hungry");
+}
+		// 11. If more than 20 seconds have elapsed, tell them the turtle died
+		// of hunger!
+if(wasHereBefore(tortoiseLocationX, tortoiseLocationY)){
+	Tortoise.moveTo((int)(getFirstLocation().getX()), (int)(getFirstLocation().getY()));
+}
+		// 12. If the Tortoise crosses it's own path, tell them they failed and
+		// move them back to the beginning
+
+	}
+
+	private long getTimeElapsed() {
+		Date currentTime = new Date();
+		return (currentTime.getTime() - startTime.getTime()) / 1000;
+	}
+
+	private void setBackgroundColor(Color color) {
+		Tortoise.getBackgroundWindow().setBackground(color);
+	}
+
+	private void showCookie() {
+		Tortoise.getBackgroundWindow().addPaintable(new Paintable() {
+			public void paint(Graphics2D g, JPanel caller) {
+				g.drawOval(cookieX, cookieY, 10, 10);
+			}
+		});
+	}
+
+	private boolean wasHereBefore(int xPosition, int yPosition) {
+		if (previousLocations.contains(new Point(xPosition, yPosition)))
+			return true;
+		else
+			return false;
+	}
+
+	private Point getFirstLocation() {
+		return previousLocations.get(0);
+	}
+
+	/***********************
+	 * don't worry about the stuff under here
+	 ******************/
+
+	Date startTime = new Date();
+
+	public static void main(String[] args) {
+		TurtlePond feeder = new TurtlePond();
+		feeder.controlTheTortoise();
+		feeder.setup();
+	}
+
+	private void controlTheTortoise() {
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
+		Tortoise.show();
+		Tortoise.setPenColor(PenColors.Purples.DarkMagenta);
+		Tortoise.getBackgroundWindow().setBackground(PenColors.Grays.SlateGray);
+		Tortoise.setSpeed(10);
+	}
+
+	public boolean dispatchKeyEvent(KeyEvent e) {
+		if (e.getID() == KeyEvent.KEY_PRESSED) {
+
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+				goRight();
+			if (e.getKeyCode() == KeyEvent.VK_LEFT)
+				goLeft();
+			if (e.getKeyCode() == KeyEvent.VK_UP)
+				goUp();
+			if (e.getKeyCode() == KeyEvent.VK_DOWN)
+				goDown();
+
+			try {
+				checkForFood();
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+
+			savePosition(Tortoise.getX(), Tortoise.getY());
+		}
+		return false;
+	}
+
+	ArrayList<Point> previousLocations = new ArrayList<Point>();
+
+	private void savePosition(int xPosition, int yPosition) {
+		previousLocations.add(new Point(xPosition, yPosition));
+	}
+}
