22cd25fdf7fbf27d40ca0d19e002d0bb97025212
Nicholas Contreras <nicholasc2000@yahoo.com>
2016-05-25 20:04:27 -0700

    new
    new

--- src/GetFriends.java
+++ src/GetFriends.java
@@ -3,9 +3,15 @@ public class GetFriends {
 
 	public static void main(String[] args){
     	Classmate ella = new Classmate();
-    	Classmate kayia = new Classmate();//4. set descriptions for each Classmate
-
-   	 //5. Use your getter to print out each description
+    	Classmate kaiya = new Classmate();
+    	Classmate katherine = new Classmate();//4. set descriptions for each Classmate
+    	katherine.setDescription("Likes ");
+    	ella.setDescription("Likes POTATOES a LOT");
+    	kaiya.setDescription("Likes pink fluffy unicorns");
+    	
+    	System.out.println("Ella " + ella.getDescription());
+    	System.out.println("Kaiya " + kaiya.getDescription());
+    	System.out.println("Katherine " + katherine.getDescription());
 	}
 
 }
@@ -13,7 +19,13 @@ public class GetFriends {
 class Classmate{
     String description;
     
+    public void setDescription(String description){
+    	this.description = description;
+    }
     
+    public String getDescription(){
+    	return description;
+    }
     //1. create a setter that sets a description this person
     //2. create a getter that gets a description of this person
 
